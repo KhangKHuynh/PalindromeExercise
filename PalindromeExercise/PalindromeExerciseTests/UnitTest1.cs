8db1032e378f538981c841074545de040f74c645
Khang Huynh <kkyhuynh@gmail.com>
2025-03-07 15:01:40 -0600

    turnin

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void IsPalindrome(string word, bool expected)
         {
-
+            //Arrange 
+            var test = new WordSmith();
+            
+            //Act
+            var actual = test.IsAPalindrome(word);
+            
+            //Assert
+            Assert.Equal(expected, actual);
+            
         }
     }
 }
